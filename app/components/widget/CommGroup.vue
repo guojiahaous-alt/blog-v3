2f87de6cc55a9f378abea1846a9a6ec5bae8beae
YuBoGuo <guojiahao.us@gmail.com>
2026-05-24 21:45:44 +0800

    博客更新ing

--- app/components/widget/CommGroup.vue
+++ app/components/widget/CommGroup.vue
@@ -2,16 +2,16 @@
 <BlogWidget
 	card
 	dim
-	title="博客/技术社区"
-	bg-img="https://p.qlogo.cn/gh/169994096/169994096/100/"
+	title="技术/问题交流"
+	bg-img=""
 	bg-right
 >
 	<div class="title text-creative">
-		纸网接入点
+		个人qq号
 	</div>
 
 	<Tip copy icon :tip-options="{ placement: 'left' }">
-		<Icon name="ri:qq-fill" />169994096
+		<Icon name="ri:qq-fill" />3108917128
 	</Tip>
 </BlogWidget>
 </template>
